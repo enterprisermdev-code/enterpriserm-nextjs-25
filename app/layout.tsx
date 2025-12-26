d8426fa72e98e804dd4fe17ca839a6d697d02a99
Riyas Al Mohamed <riyas.hs2021@gmail.com>
2025-12-26 10:28:58 +0530

    GTM-Search Console code added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Space_Grotesk, Manrope } from "next/font/google";
+
+import Script from "next/script";
 import "./globals.css";
 import CookieConsent from "@/components/cookie-consent";
 
@@ -27,22 +29,31 @@ export default function RootLayout({
     <html lang="en">
       <head>
         {/* Google Tag Manager */}
-        <script dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5BF9T656');`
-        }} />
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5BF9T656');
+          `}
+        </Script>
         {/* End Google Tag Manager */}
+        <meta name="google-site-verification" content="z8l8bUTe8eH0WfDFUvqiygPVKVfh93MeUi2bewIhEpY" />
       </head>
       <body
         className={`${spaceGrotesk.variable} ${manrope.variable} antialiased`}
         suppressHydrationWarning
       >
         {/* Google Tag Manager (noscript) */}
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5BF9T656"
-height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5BF9T656"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         {/* End Google Tag Manager (noscript) */}
         {children}
         <CookieConsent />
